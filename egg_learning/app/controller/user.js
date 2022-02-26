54cff6fbdbb0bfa874abfa7cc0d410794f756e25
keeko <787018661@qq.com>
2022-02-26 10:47:31 +0800

    [demo] egg controller post请求ctx.request.body

--- egg_learning/app/controller/user.js
+++ egg_learning/app/controller/user.js
@@ -22,13 +22,26 @@ class UserController extends Controller {
   async detail() {
     const { ctx } = this;
     // console.log("ctx.query->", ctx.query); // 如 {id: 100}
+    // query ↓ 对应query型 /user?id=2
     ctx.body = ctx.query.id;
   }
 
   async detail2() {
     const { ctx } = this;
     // console.log("ctx.params->", ctx.params); // 如 {id: 200}
+    // params ↓ 对应restful型路由 /user/:id  -> /user/3
     ctx.body = ctx.params.id;
+  }
+
+  async add() {
+    const { ctx } = this;
+
+    // ctx.body = "add";
+    ctx.body = {
+      code: 0,
+      data: ctx.request.body,
+      msg: '',
+    };
   }
 
 }
