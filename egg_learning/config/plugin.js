04bba706e4dfee723815146bb36f7eebf20f6d9d
Keeko <787018661@qq.com>
2022-03-16 17:12:12 +0800

    [demo] egg-mysql 配置02

--- egg_learning/config/plugin.js
+++ egg_learning/config/plugin.js
@@ -3,12 +3,12 @@
 const path = require('path');
 
 /** @type Egg.EggPlugin */
-module.exports = {
+// module.exports = {
   // had enabled by egg
   // static: {
   //   enable: true,
   // }
-};
+// };
 
 exports.auth = {
   enable: false,
