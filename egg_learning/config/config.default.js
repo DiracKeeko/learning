d721e6e11e5cb079ddeb3e5cff79ac01d9df6e25
keeko <787018661@qq.com>
2022-03-18 20:19:22 +0800

    [demo] egg 插件配置及使用

--- egg_learning/config/config.default.js
+++ egg_learning/config/config.default.js
@@ -1,28 +1,28 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
-const path = require('path');
+"use strict";
+const path = require("path");
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1645458417257_5743';
+  config.keys = appInfo.name + "_1645458417257_5743";
 
   // add your middleware config here
   config.middleware = ["httpLog"];
 
   // ↓ 配置 httpLog middleware options
   config.httpLog = {
-    type: "test"
-  }
+    type: "test",
+  };
 
   config.security = {
     csrf: {
@@ -32,7 +32,7 @@ module.exports = appInfo => {
 
   config.view = {
     mapping: {
-      ".html": "ejs" // 以.html结尾的文件用ejs模板引擎渲染
+      ".html": "ejs", // 以.html结尾的文件用ejs模板引擎渲染
     },
     root: path.join(appInfo.baseDir, "app/html"), // 配置模板引擎的根目录为app/html
     // ↓ 模板多目录配置
@@ -43,19 +43,23 @@ module.exports = appInfo => {
   };
   config.ejs = {
     // delimiter: "$" // 定界符<$= id $>, 这里修改是修改全局的定界符
-    delimiter: "%"
+    delimiter: "%",
   };
 
   config.static = {
     prefix: "/assets/", // 静态资源路径默认前缀
-    dir: path.join(appInfo.baseDir, "app/assets") // 静态资源放置路径
+    dir: path.join(appInfo.baseDir, "app/assets"), // 静态资源放置路径
   };
 
   config.session = {
     key: "KEEKO_SESS", // 设置session的key, 默认值是EGG_SESS
     httpOnly: true,
     maxAge: 1000 * 60, // 60秒过期
-    renew: true // 在过期时间到1/2 maxAge的时候如果再次访问，会重置maxAge
+    renew: true, // 在过期时间到1/2 maxAge的时候如果再次访问，会重置maxAge
+  };
+
+  config.auth = {
+    exclude: ["/home", "/user", "/login", "/logout"],
   };
 
   config.mysql = {
@@ -66,9 +70,9 @@ module.exports = appInfo => {
       port: "3306",
       user: "root",
       password: "root",
-      database: "egg"
-    }
-  }
+      database: "egg",
+    },
+  };
 
   // add your user config here
   const userConfig = {
