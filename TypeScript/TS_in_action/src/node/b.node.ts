b1c0416b992813cc85577b2ae2455f4cc2f52ff8
Keeko <787018661@qq.com>
2022-05-19 14:42:16 +0800

    [chore] TS中的commonjs模块系统

---
+++ TypeScript/TS_in_action/src/node/b.node.ts
@@ -0,0 +1,16 @@
+// exports === module.exports
+// 导出多个变量
+
+// module.exports = {}
+
+/*
+  在commonjs模块系统中
+
+  module.exports被视为顶级导出
+  exports.x 被视为次级导出
+
+  顶级导出会覆盖次级导出(无论位置先后)
+*/
+
+exports.c = 3
+exports.d = 4
