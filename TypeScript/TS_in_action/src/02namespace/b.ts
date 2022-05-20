666ccf5dc52e9d05bf1524dc52266c015f7eaabf
Keeko <787018661@qq.com>
2022-05-20 19:06:07 +0800

    [chore] TS 命名空间别名

--- TypeScript/TS_in_action/src/02namespace/b.ts
+++ TypeScript/TS_in_action/src/02namespace/b.ts
@@ -10,8 +10,15 @@ namespace Shape {
 console.log(Shape.cricle(2))
 console.log(Shape.square(2))
 
-import cricle = Shape.cricle
+// 命名空间别名 -> import 
+import cricle = Shape.cricle; // 命名空间别名
 console.log(cricle(2))
 
 // tsc b.ts  -> a.js + b.js
 // index.html中  src引入 a.js 以及 b.js (.js是编译后的产物)
+
+/* 
+  命名空间的本质是闭包 （见编译产物 a.js 的实现）
+  在ESM模块化规范之后，命名空间基本可以不使用
+  TS保留命名空间是对JS的兼容
+*/
