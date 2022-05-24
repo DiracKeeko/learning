2c9aea1d70dc5bccdb18132b2a9178a04c66f1dc
Keeko <787018661@qq.com>
2022-05-24 19:41:28 +0800

    [chore] TS工程引用

---
+++ TypeScript/TS_in_action/src/07project-references/buildDescription.js
@@ -0,0 +1,18 @@
+/* 
+  进入new目录
+  命令行输入
+    tsc -b src/server
+    tsc -b src/server --verbose (带上编译信息)
+
+    tsc -b src/client
+    tsc -b src/client --verbose
+    
+    tsc -b test
+    tsc -b test --verbose
+
+
+  说明:
+    -b 是build的简写
+
+    tsc -b test --clean 清空编译文件
+*/
