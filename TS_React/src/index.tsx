df90e562671a274958bfad3355c263bf5fe8094b
keeko <787018661@qq.com>
2022-05-28 15:30:25 +0800

    [chore] 手动创建TS_React项目

---
+++ TS_React/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './components/Hello';
+
+ReactDOM.render(
+    <Hello name="TypeScript React" />,
+    document.querySelectorAll('.app')[0]
+);
