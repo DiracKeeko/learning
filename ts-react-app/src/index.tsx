563c308eefe76fb4fa4a20948bec97616502ef60
Keeko <787018661@qq.com>
2022-05-31 10:28:22 +0800

    [feat] React类组件的创建和使用

--- ts-react-app/src/index.tsx
+++ ts-react-app/src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './components/demo/Hello';
-// import HelloClass from './components/demo/HelloClass';
+// import Hello from './components/demo/Hello';
+import HelloClass from './components/demo/HelloClass';
 // import HelloHOC from './components/demo/HelloHOC';
 // import HelloHooks from './components/demo/HelloHooks';
 // import App from './components/App';
 // import Root from './routers';
 
 ReactDOM.render(
-    <Hello name="TypeScript" />,
-    // <HelloClass name="TypeScript" />,
+    // <Hello name="TypeScript func" />,
+    <HelloClass name="TypeScript class" />,
     // <HelloHOC name="TypeScript" loading={false} />,
     // <HelloHooks name="TypeScript" />,
     // <App />,
